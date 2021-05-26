5d6cbba47506e762c3ff7f3fc257f5be52265f36
Mathias <mathias.richard.schneider@gmail.com>
2021-05-25 22:50:49 -0500

    adds genre and movie_genre models. adds associations so that you can call the genres of a movie or the movies associated with a genre.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,16 @@
 class Movie < ApplicationRecord
 
   has_many :actors
-  
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
   validates :title, presence: true, uniqueness: true
   validates :year, numericality: {greater_than: 1888}
   validates :plot, length: {in: 10..500}
+
+  # def genre_names
+  #   genres_array = []
+  #   genres_array << :genres[0]
+  #   return genres_array
+  # end
 end
